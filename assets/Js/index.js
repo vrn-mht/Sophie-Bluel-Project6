c2902501374d15e3354a70ef36e8a09a75424748
Veron Mahamat <oumar.mht243@gmail.com>
2024-01-02 14:01:07 +0100

    Ajout de la page login

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,15 +1,15 @@
 const gallery = document.querySelector(".gallery");
+const filters = document.querySelector(".filters");
 
 async function getWorks() {
-    const response = await fetch("http://localhost:5678/api/works")
+    const response = await fetch("http://localhost:5678/api/works");
     return await response.json();
 }
-getWorks();
 
-
-async function affichageWorks() {
-    const arrayWorks = await getWorks();
-    arrayWorks.forEach((works) => {
+// Affichage des works dans le DOM //
+async function affichageWorks(worksArray) {
+    gallery.innerHTML = ""; // Vider la galerie avant d'ajouter de nouveaux éléments
+    worksArray.forEach((works) => {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -20,4 +20,50 @@ async function affichageWorks() {
         gallery.appendChild(figure);
     });
 }
-affichageWorks();
+
+// Affichage des boutons par catégories //
+
+// Récupération du tableau des catégories //
+async function getCategorys() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    return await response.json();
+}
+
+async function displayButtonsCategorys() {
+    const categorys = await getCategorys();
+    categorys.forEach(category => {
+        const btn = document.createElement("button")
+        btn.textContent = category.name.toUpperCase();
+        btn.id = category.id;
+        filters.appendChild(btn);
+    });
+}
+
+// Filtrage au clic sur le bouton par catégories //
+
+async function filterCategory() {
+    const images = await getWorks();
+    console.log(images);
+    const buttons = document.querySelectorAll(".filters button")
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            const btnId = e.target.id;
+            if (btnId !== "0") {
+                const buttonsTriCategory = images.filter((caption) => {
+                    return caption.categoryId == btnId
+                });
+                affichageWorks(buttonsTriCategory);
+            } else {
+                affichageWorks(images);
+            }
+            console.log(btnId);
+        })
+    });
+}
+
+// Appeler les fonctions après le chargement de la page
+document.addEventListener("DOMContentLoaded", async () => {
+    await displayButtonsCategorys();
+    await affichageWorks(await getWorks()); // Afficher toutes les œuvres par défaut
+    await filterCategory();
+});
---
+++ assets/Js/login.js
@@ -0,0 +1,2 @@
+const email = document.querySelector("form #email")
+const password = document.querySelector("form #password")
